9d9d391676dd8b5ba1a0c85a9b18a30d07eebd38
VladimirArbitr <bragin.arbitr@icloud.com>
2024-11-22 21:02:41 +0300

    Fix missing method should_be_login_link in MainPage

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,21 +1,14 @@
 from .base_page import BasePage
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
 
 
 class MainPage(BasePage):
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")  # локатор для логин-ссылки
-
-    def should_be_login_link(self):
-        assert self.is_element_present(*self.LOGIN_LINK), "Login link is not presented"
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")  # Локатор для ссылки
 
     def go_to_login_page(self):
-        login_link = self.browser.find_element(*self.LOGIN_LINK)  # используем локатор
+        login_link = self.browser.find_element(*self.LOGIN_LINK)
         login_link.click()
 
-    def is_element_present(self, how, what):
-        try:
-            self.browser.find_element(how, what)
-            return True
-        except NoSuchElementException:
-            return False
+    def should_be_login_link(self):
+        # Проверяем, что элемент с локатором LOGIN_LINK присутствует на странице
+        assert self.is_element_present(*self.LOGIN_LINK), "Login link is not presented"
