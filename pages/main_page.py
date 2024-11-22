09dfc4bb50ee9fb8ecc1b1d19ba2b1f83a5ac585
VladimirArbitr <bragin.arbitr@icloud.com>
2024-11-22 20:21:26 +0300

    Добавлен метод go_to_login_page в MainPage и обновлён тест

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,5 +1,11 @@
 from .base_page import BasePage
 
+from selenium.webdriver.common.by import By
+
 
 class MainPage(BasePage):
-    pass
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")  # локатор для логин-ссылки
+
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(*self.LOGIN_LINK)  # используем локатор
+        login_link.click()
--- test_main_page.py
+++ test_main_page.py
@@ -1,12 +1,9 @@
-from selenium.webdriver.common.by import By
-
 from pages.main_page import MainPage
 
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)  # создаём объект страницы
-    page.open()  # открываем страницу
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")  # заменим позже на метод из Page Object
-    login_link.click()
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()  # используем метод из Page Object
 
