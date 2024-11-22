27f11b5963789d326612506a8b9241b9865d4103
VladimirArbitr <bragin.arbitr@icloud.com>
2024-11-22 15:59:07 +0300

    Обновлён тест с использованием MainPage

--- test_main_page.py
+++ test_main_page.py
@@ -1,7 +1,12 @@
 from selenium.webdriver.common.by import By
 
+from pages.main_page import MainPage
+
+
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
+    page = MainPage(browser, link)  # создаём объект страницы
+    page.open()  # открываем страницу
+    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")  # заменим позже на метод из Page Object
     login_link.click()
+
